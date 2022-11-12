7512e5d59786f5e802a5746da8443038b84de90c
Victor Manuel Cordero <cordero.victor@gmail.com>
2022-11-11 22:00:36 -0300

    Clase 11/11

--- ConsoleIMC/Program.cs
+++ ConsoleIMC/Program.cs
@@ -1,11 +1,13 @@
-﻿namespace ConsoleIMC
+﻿using System.Diagnostics;
+
+namespace ConsoleIMC
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string NombrePaciente;
-            char Genero;
+            string NombrePaciente = string.Empty;
+            char Genero='F';
             double Peso = 0;
             double Altura = 0;
             double IMC = 0;
@@ -17,38 +19,101 @@
              de calcular su IMC
             */
 
-            System.Console.WriteLine("Indica tu nombre y apellido:");
-            NombrePaciente = System.Console.ReadLine();
+            //Genero un Menu
+            byte opcion;
+            do
+            {
+                Console.WriteLine("Bienvenido IMC V beta 1.0");
+                Console.WriteLine("=========================");
+                Console.WriteLine("1 - Nuevo Diagnostico");
+                Console.WriteLine("2 - Salir");
+                opcion = byte.Parse(Console.ReadLine());
 
-            System.Console.WriteLine("Indica tu peso:");
-            //Peso = float.Parse(System.Console.ReadLine());
-            Peso = Convert.ToDouble(System.Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1: 
+                        System.Console.WriteLine("Indica tu nombre y apellido:");
+                        NombrePaciente = System.Console.ReadLine();
 
-            System.Console.WriteLine("Indica tu Altura:");
-            Altura = Convert.ToDouble(System.Console.ReadLine());
+                        System.Console.WriteLine("Indica tu Genero (F/M):");
+                        Genero = char.Parse(System.Console.ReadLine());
 
-            //Aca se calcula el IMC
+                        System.Console.WriteLine("Indica tu peso:");
+                        //Peso = float.Parse(System.Console.ReadLine());
+                        Peso = Convert.ToDouble(System.Console.ReadLine());
+
+                        System.Console.WriteLine("Indica tu Altura:");
+                        Altura = Convert.ToDouble(System.Console.ReadLine());
+
+                        //Aca se calcula el IMC
+                        IMC = CalcularIMC(Peso,Altura);
+
+                        //Se muestra el resultado
+                        System.Console.WriteLine("Tu IMC es: " + IMC);
+
+                        //Le brindamos el diagnostico al Paciente
+                        System.Console.WriteLine($"El diagnóstico es: { ObtenerDiagnostico(IMC) } ");
+                        //System.Console.WriteLine("El diagnótico es: " + ObtenerDiagnostico(IMC)); //Concatenación
+                        break;
+                }
+
+            } while (opcion != 2);
+
+            Console.WriteLine($"Hasta luego { GetTitulo(Genero) } { NombrePaciente } , cuidese.");
+
+        }
+
+        //Implementar el método que calcula el IMC
+        static double CalcularIMC(double Peso, double Altura)
+        {
+            double IMC;
             IMC = Peso / (Altura * Altura);
+            return IMC;
+        }
 
-            //Se muestra el resultado
-            System.Console.WriteLine("Tu IMC es: " + IMC);
+        //Devolver si es Sr o Sra
+        static string GetTitulo (char Genero)
+        {
+            string Titulo;
+            switch (Genero)
+            {
+                case 'F':
+                    Titulo = "Sra.";
+                    break;
+                case 'M':
+                    Titulo = "Sr";
+                    break;
+                default:
+                    Titulo = "X";
+                    break;
+            }
+            return Titulo;
+        }
 
-            //Le brindamos el diagnostico al Paciente
+
+        //Implementar el método para retornar el diagnóstico
+        static string ObtenerDiagnostico(double IMC)
+        {
+            string Diagnostico;
             if (IMC <= 18.5)
             {
-                Console.WriteLine("Tienes Bajo peso");
-            }else if (IMC > 18.5 && IMC <= 24.9)
+                Diagnostico = "Tienes Bajo peso";
+            }
+            else if (IMC > 18.5 && IMC <= 24.9)
             {
-                Console.WriteLine("Tienes un Peso saludable");
-            }else if (IMC > 25 && IMC <= 29.9)
+                Diagnostico = "Tienes un Peso saludable";
+            }
+            else if (IMC > 25 && IMC <= 29.9)
             {
-                Console.WriteLine("Tienes Sobrepeso");
+                Diagnostico = "Tienes Sobrepeso";
             }
             else
             {
-                Console.WriteLine("Tienes Obesidad, ve corriendo al hospital");
+                Diagnostico = "Tienes Obesidad, ve corriendo al hospital";
             }
 
+            return Diagnostico;
         }
+
     }
 }
